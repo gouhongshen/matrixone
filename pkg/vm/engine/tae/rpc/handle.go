0f091ca3998e5b841c59fa43fb8fd1da0bdaf6e0
gouhongshen <gouhongshen@hotmail.com>
2023-12-06 15:37:22 +0800

    add magic number

--- pkg/vm/engine/tae/rpc/handle.go
+++ pkg/vm/engine/tae/rpc/handle.go
@@ -1179,7 +1179,10 @@ func (h *Handle) HandleStorageUsage(ctx context.Context, meta txn.TxnMeta,
 	memo := logtail.GetTNUsageMemo()
 
 	memo.EnterProcessing()
-	defer memo.LeaveProcessing()
+	defer func() {
+		resp.Magic = logtail.StorageUsageMagic
+		memo.LeaveProcessing()
+	}()
 
 	if !memo.HasUpdate() {
 		resp.Succeed = true
