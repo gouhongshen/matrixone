9877f93d7dcc87f4298369cee9b79dcb3254cbf2
Wenbin1002 <waterdance1002@gmail.com>
2024-09-27 16:37:40 +0800

    read object stats

--- pkg/vm/engine/migrate/migrate_test.go
+++ pkg/vm/engine/migrate/migrate_test.go
@@ -3,49 +3,48 @@ package db
 import (
 	"testing"
 
-	"github.com/matrixorigin/matrixone/pkg/container/types"
 	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/blockio"
-	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/catalog"
-	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/txn/txnbase"
 )
 
 func TestXxx(t *testing.T) {
 	blockio.Start("")
 	defer blockio.Stop("")
-	fs := NewFileFs("/root/matrixone/mo-data/shared")
+	fs := NewFileFs("/home/v/mo/matrixone/mo-data/shared")
 	// 1. ReadCkp11File
-	fromEntry, ckpbats := ReadCkp11File(fs, "ckp11/meta_0-0_1727249945669124518-1.ckp")
+	fromEntry, ckpbats := ReadCkp11File(fs, "ckp/meta_0-0_1727232610822354759-0.ckp")
 	t.Log(fromEntry.String())
 
-	// 2. Replay To 1.3 catalog
-	cata := ReplayCatalogFromCkpData11(ckpbats)
+	ReplayObjectBatch(ckpbats[ObjectInfoIDX])
 
-	// 3. Dump catalog to 3 tables batch
-	bDb, bTbl, bCol := DumpCatalogToBatches(cata)
-
-	// 4. Sink and get object stats
-	objDB := SinkBatch(catalog.SystemDBSchema, bDb, fs)
-	objTbl := SinkBatch(catalog.SystemTableSchema, bTbl, fs)
-	objCol := SinkBatch(catalog.SystemColumnSchema, bCol, fs)
-
-	// 5. Write 1.3 Global Ckp
-	txnNode := &txnbase.TxnMVCCNode{
-		Start:   types.BuildTS(42424242, 0),
-		Prepare: types.BuildTS(42424243, 0),
-		End:     types.BuildTS(42424243, 0),
-	}
-	entryNode := &catalog.EntryMVCCNode{
-		CreatedAt: types.BuildTS(42424243, 0),
-	}
-	RewriteCkp(fs, fromEntry, txnNode, entryNode, objDB, objTbl, objCol)
-
-	for _, v := range objDB {
-		t.Log(v.String())
-	}
-	for _, v := range objTbl {
-		t.Log(v.String())
-	}
-	for _, v := range objCol {
-		t.Log(v.String())
-	}
+	//// 2. Replay To 1.3 catalog
+	//cata := ReplayCatalogFromCkpData11(ckpbats)
+	//
+	//// 3. Dump catalog to 3 tables batch
+	//bDb, bTbl, bCol := DumpCatalogToBatches(cata)
+	//
+	//// 4. Sink and get object stats
+	//objDB := SinkBatch(catalog.SystemDBSchema, bDb, fs)
+	//objTbl := SinkBatch(catalog.SystemTableSchema, bTbl, fs)
+	//objCol := SinkBatch(catalog.SystemColumnSchema, bCol, fs)
+	//
+	//// 5. Write 1.3 Global Ckp
+	//txnNode := &txnbase.TxnMVCCNode{
+	//	Start:   types.BuildTS(42424242, 0),
+	//	Prepare: types.BuildTS(42424243, 0),
+	//	End:     types.BuildTS(42424243, 0),
+	//}
+	//entryNode := &catalog.EntryMVCCNode{
+	//	CreatedAt: types.BuildTS(42424243, 0),
+	//}
+	//RewriteCkp(fs, fromEntry, txnNode, entryNode, objDB, objTbl, objCol)
+	//
+	//for _, v := range objDB {
+	//	t.Log(v.String())
+	//}
+	//for _, v := range objTbl {
+	//	t.Log(v.String())
+	//}
+	//for _, v := range objCol {
+	//	t.Log(v.String())
+	//}
 }
