cf1f45687adbb78eeff299066d6ac55aa18cfedb
gouhongshen <gouhongshen@hotmail.com>
2024-10-08 19:08:34 +0800

    replay deletes

--- pkg/vm/engine/migrate/migrate_test.go
+++ pkg/vm/engine/migrate/migrate_test.go
@@ -1,20 +1,22 @@
 package db
 
 import (
-	"github.com/matrixorigin/matrixone/pkg/container/types"
-	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/catalog"
-	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/txn/txnbase"
+	"path"
 	"testing"
 
+	"github.com/matrixorigin/matrixone/pkg/container/types"
 	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/blockio"
+	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/catalog"
+	"github.com/matrixorigin/matrixone/pkg/vm/engine/tae/txn/txnbase"
 )
 
 func TestXxx(t *testing.T) {
 	blockio.Start("")
 	defer blockio.Stop("")
-	fs := NewFileFs("/home/v/mo/matrixone/mo-data/shared")
+
+	oldDataFS := NewFileFs(path.Join(rootDir, "shared"))
 	// 1. ReadCkp11File
-	fromEntry, ckpbats := ReadCkp11File(fs, "ckp11/meta_0-0_1727659886661208596-1.ckp")
+	fromEntry, ckpbats := ReadCkp11File(oldDataFS, "ckp/meta_0-0_1728382922337692000-1.ckp")
 	t.Log(fromEntry.String())
 
 	// 2. Replay To 1.3 catalog
@@ -23,10 +25,12 @@ func TestXxx(t *testing.T) {
 	// 3. Dump catalog to 3 tables batch
 	bDb, bTbl, bCol := DumpCatalogToBatches(cata)
 
+	newDataFS := NewFileFs(path.Join(rootDir, "rewritten"))
+
 	// 4. Sink and get object stats
-	objDB := SinkBatch(catalog.SystemDBSchema, bDb, fs)
-	objTbl := SinkBatch(catalog.SystemTableSchema, bTbl, fs)
-	objCol := SinkBatch(catalog.SystemColumnSchema, bCol, fs)
+	objDB := SinkBatch(catalog.SystemDBSchema, bDb, newDataFS)
+	objTbl := SinkBatch(catalog.SystemTableSchema, bTbl, newDataFS)
+	objCol := SinkBatch(catalog.SystemColumnSchema, bCol, newDataFS)
 
 	//5. Write 1.3 Global Ckp
 	txnNode := &txnbase.TxnMVCCNode{
@@ -37,7 +41,8 @@ func TestXxx(t *testing.T) {
 	entryNode := &catalog.EntryMVCCNode{
 		CreatedAt: types.BuildTS(42424243, 0),
 	}
-	RewriteCkp(fs, fromEntry, ckpbats, txnNode, entryNode, objDB, objTbl, objCol)
+
+	RewriteCkp(oldDataFS, newDataFS, fromEntry, ckpbats, txnNode, entryNode, objDB, objTbl, objCol)
 
 	for _, v := range objDB {
 		t.Log(v.String())
